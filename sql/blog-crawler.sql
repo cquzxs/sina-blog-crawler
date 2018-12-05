8ed53625dcb3746f77d3ff042c4b1efc699885ba
zengxueshan <zengxueshan@yun-kai.com>
2018-12-05 17:04:59 +0800

    更新sql脚本

--- sql/blog-crawler.sql
+++ sql/blog-crawler.sql
@@ -10,9 +10,9 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-12-04 17:42:37
+Date: 2018-12-05 17:04:08
 */
-use blog-crawler;
+
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -29,6 +29,20 @@ CREATE TABLE `blog_user_info` (
   `follow_count` int(255) DEFAULT NULL,
   `fans_count` int(255) DEFAULT NULL,
   `blog_count` int(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for ip_proxy
+-- ----------------------------
+DROP TABLE IF EXISTS `ip_proxy`;
+CREATE TABLE `ip_proxy` (
+  `id` varchar(255) NOT NULL,
+  `ip` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `response_speed` varchar(255) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
